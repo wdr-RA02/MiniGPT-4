c48b30aeb9c230a65c352d2db91fc2ad8e7b8c2a
wdr-RA02 <cydia1829@gmail.com>
2023-10-17 10:09:00 +0000

    gitignore and minor fix

--- minigpt4/models/minigpt_base.py
+++ minigpt4/models/minigpt_base.py
@@ -220,7 +220,7 @@ class MiniGPTBase(BaseModel):
             conv_q, conv_a = samples['conv_q'], samples['conv_a']
 
             connect_sym = samples['connect_sym'][0]
-            conv_q = [q.split(connect_sym)for q in conv_q]
+            conv_q = [q.split(connect_sym) for q in conv_q]
             conv_a = [a.split(connect_sym) for a in conv_a]
 
             conv_q = [[self.prompt_template.format(item) for item in items] for items in conv_q]
@@ -236,7 +236,7 @@ class MiniGPTBase(BaseModel):
             else:
                 instruction = None
 
-            if self.chat_template:
+            if self.get("chat_template", None):
                 instruction = [self.prompt_template.format(instruct) for instruct in instruction]
 
             if 'length' in samples:
@@ -315,72 +315,72 @@ class MiniGPTBase(BaseModel):
         return embeds
 
 
-    @torch.no_grad()
-    def generate(
-        self,
-        images,
-        texts,
-        num_beams=1,
-        max_new_tokens=20,
-        min_length=1,
-        top_p=0.9,
-        repetition_penalty=1,
-        length_penalty=1,
-        temperature=1,
-        do_sample=False,
-        stop_words_ids=[2],
-    ):
-        '''
-            function for generate test use
-        '''
+    # @torch.no_grad()
+    # def generate(
+    #     self,
+    #     images,
+    #     texts,
+    #     num_beams=1,
+    #     max_new_tokens=20,
+    #     min_length=1,
+    #     top_p=0.9,
+    #     repetition_penalty=1,
+    #     length_penalty=1,
+    #     temperature=1,
+    #     do_sample=False,
+    #     stop_words_ids=[2],
+    # ):
+    #     '''
+    #         function for generate test use
+    #     '''
 
-        # stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(
-        #     stops=[torch.tensor([i]).to(self.device) for i in stop_words_ids])])
+    #     # stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(
+    #     #     stops=[torch.tensor([i]).to(self.device) for i in stop_words_ids])])
 
-        img_embeds, atts_img = self.encode_img(images.to(self.device))
-        image_lists = [[image_emb[None]] for image_emb in img_embeds]
+    #     img_embeds, atts_img = self.encode_img(images.to(self.device))
+    #     image_lists = [[image_emb[None]] for image_emb in img_embeds]
 
-        batch_embs = [self.get_context_emb(text, img_list) for text, img_list in zip(texts, image_lists)]
+    #     batch_embs = [self.get_context_emb(text, img_list) for text, img_list in zip(texts, image_lists)]
 
-        batch_size = len(batch_embs)
-        max_len = max([emb.shape[1] for emb in batch_embs])
-        emb_dim = batch_embs[0].shape[2]
-        dtype = batch_embs[0].dtype
-        device = batch_embs[0].device
+    #     batch_size = len(batch_embs)
+    #     max_len = max([emb.shape[1] for emb in batch_embs])
+    #     emb_dim = batch_embs[0].shape[2]
+    #     dtype = batch_embs[0].dtype
+    #     device = batch_embs[0].device
 
-        embs = torch.zeros([batch_size, max_len, emb_dim], dtype=dtype, device=device)
-        attn_mask = torch.zeros([batch_size, max_len], dtype=torch.int, device=device)
-        for i, emb in enumerate(batch_embs):
-            emb_len = emb.shape[1]
-            embs[i, -emb_len:] = emb[0]
-            attn_mask[i, -emb_len:] = 1
+    #     embs = torch.zeros([batch_size, max_len, emb_dim], dtype=dtype, device=device)
+    #     attn_mask = torch.zeros([batch_size, max_len], dtype=torch.int, device=device)
+    #     for i, emb in enumerate(batch_embs):
+    #         emb_len = emb.shape[1]
+    #         embs[i, -emb_len:] = emb[0]
+    #         attn_mask[i, -emb_len:] = 1
 
-        with self.maybe_autocast():
-            outputs = self.llama_model.generate(
-                inputs_embeds=embs,
-                attention_mask=attn_mask,
-                max_new_tokens=max_new_tokens,
-                num_beams=num_beams,
-                length_penalty=length_penalty,
-                temperature=temperature,
-                do_sample=do_sample,
-                min_length=min_length,
-                top_p=top_p,
-                repetition_penalty=repetition_penalty,
-                stopping_criteria=stopping_criteria,
-            )
+    #     with self.maybe_autocast():
+    #         outputs = self.llama_model.generate(
+    #             inputs_embeds=embs,
+    #             attention_mask=attn_mask,
+    #             max_new_tokens=max_new_tokens,
+    #             num_beams=num_beams,
+    #             length_penalty=length_penalty,
+    #             temperature=temperature,
+    #             do_sample=do_sample,
+    #             min_length=min_length,
+    #             top_p=top_p,
+    #             repetition_penalty=repetition_penalty,
+    #             stopping_criteria=stopping_criteria,
+    #         )
 
-        answers = []
-        for output_token in outputs:
-            if output_token[0] == 0:
-                output_token = output_token[1:]
-            output_texts = self.llama_tokenizer.decode(output_token, skip_special_tokens=True)
-            output_texts = output_texts.split('</s>')[0]  # remove the stop sign </s>
-            output_texts = output_texts.replace("<s>", "")
-            output_texts = output_texts.split(r'[/INST]')[-1].strip()
-            answers.append(output_texts)
+    #     answers = []
+    #     for output_token in outputs:
+    #         if output_token[0] == 0:
+    #             output_token = output_token[1:]
+    #         output_texts = self.llama_tokenizer.decode(output_token, skip_special_tokens=True)
+    #         output_texts = output_texts.split('</s>')[0]  # remove the stop sign </s>
+    #         output_texts = output_texts.replace("<s>", "")
+    #         output_texts = output_texts.split(r'[/INST]')[-1].strip()
+    #         answers.append(output_texts)
 
-        return answers
+    #     return answers
 
     @torch.no_grad()
     def multi_select(self, images, texts, answers, num_cand=None):
