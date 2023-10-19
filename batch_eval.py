e4cae7e026f211a072171e2aeb6c3bc35ba5b7d7
wdr-RA02 <cydia1829@gmail.com>
2023-10-19 11:59:24 +0000

    add inference based on pcap

---
+++ batch_eval.py
@@ -0,0 +1,133 @@
+import argparse
+import random
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+
+from transformers import StoppingCriteriaList
+
+from minigpt4.common.config import Config
+from minigpt4.common.dist_utils import get_rank
+from minigpt4.common.registry import registry
+from minigpt4.conversation.cli_infer import (MiniGPT4_CLIGenerator as BaseCLIGen, 
+                                             StoppingCriteriaSub)
+from minigpt4.datasets.datasets.personality_captions.utils import img_hash_to_addr
+
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+from typing import List, Union, Dict
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="CLI Evaluator")
+    parser.add_argument("--cfg-path", required=True, help="path to configuration file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
+    parser.add_argument(
+        "--options",
+        nargs="+",
+        help="override some settings in the used config, the key-value pair "
+        "in xxx=yyy format will be merged into config file (deprecate), "
+        "change to --cfg-options instead.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def setup_seeds(config):
+    seed = config.run_cfg.seed + get_rank()
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+
+
+# ========================================
+#             Model Initialization
+# ========================================
+
+instruction_dict = {
+    'pretrain_vicuna0': 
+        "Give the following image: <Img>ImageContent</Img>. "
+        "You will be able to see the image once I provide it to you. Please answer my questions. "
+        "###Human: {} ###Assistant:",
+    'pretrain_llama2': 
+        "Give the following image: <Img>ImageContent</Img>. "
+        "You will be able to see the image once I provide it to you. Please answer my questions. "
+        "<s>[INST] {} [/INST] "
+}
+args = parse_args()
+cfg = Config(args)
+
+model_config = cfg.model_cfg
+model_config.device_8bit = args.gpu_id
+model_cls = registry.get_model_class(model_config.arch)
+model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+
+vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
+vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
+
+stop_words_ids = [[835], [2277, 29937], [29937]]
+stop_words_ids = [torch.tensor(ids).to(device='cuda:{}'.format(args.gpu_id)) for ids in stop_words_ids]
+stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+
+print('Initialization Finished')
+
+# this supports batch decoding
+class CLIGeneratorForPCap(BaseCLIGen):
+    def __init__(self, model, vis_processor, 
+                 instruction:str, 
+                 persona_token:str = "<persona>",
+                 device='cuda:0', stopping_criteria=None):
+        super().__init__(model, vis_processor, 
+                         instruction, device, stopping_criteria)
+        self.persona_token = persona_token
+    
+    def insert_persona(self, 
+                       prompts: Union[str, List[str]],
+                       personalities: List[str]):
+        # preprocessing: insert persona
+        bsz = len(personalities)
+        if isinstance(prompts, str):
+            prompts = [prompts] * bsz
+
+        for idx in range(bsz):
+            if not prompts[idx].startswith("<Img><ImageHere></Img>"):
+                prompts[idx] = "<Img><ImageHere></Img> " + prompts[idx].lstrip()
+
+        # make sure all prompts include <persona>
+        assert all(self.persona_token in prompt for prompt in prompts)
+
+        # replace <persona> with personality
+        prompts_persona = [prompt.replace(self.persona_token, persona) \
+                           for prompt, persona in zip(prompts, personalities)]
+        
+        return prompts_persona
+    
+if __name__=="__main__":
+    '''your code here'''
+    prompt = "Write a comment of this image in the context of a given personality trait: <persona>."
+
+    instruction = instruction_dict[model_config.model_type]
+    # instruction = "###Human: {} ###Assistant:"
+    generator = CLIGeneratorForPCap(model, vis_processor, instruction=instruction, 
+                             device='cuda:{}'.format(args.gpu_id), stopping_criteria=stopping_criteria)
+    import json
+    with open("tests/inference_src.json", "r") as f:
+        examples = json.load(f)
+    
+    # convert record to set
+    keys = list(examples[0].keys())
+    examples={k: [x[k] for x in examples] for k in keys}
+    examples_out=img_hash_to_addr(examples, "dataset/PCap/yfcc_images/","{}.jpg")
+
+    prompts = generator.insert_persona(prompt, examples_out["personality"])
+    output, output_text = generator.generate_response(prompts, 
+                                                      examples_out["images"],
+                                                      do_sample=False,
+                                                      max_length=50)
+    
+    print(output_text)
